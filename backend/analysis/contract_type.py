d7668658a0d28c4fba482e990856c9cab8b35d73
Joost Bruneel <joost.bruneel@gmail.com>
2025-04-11 09:45:54 +0200

    updates

--- backend/analysis/contract_type.py
+++ backend/analysis/contract_type.py
@@ -42,7 +42,52 @@ class ContractTypeEvaluation:
         vault = next(filter(lambda x: x.name == "digital_meter", vaults), None)
         return vault
 
+    def _get_avg_epex(self):
+        from_utc = int(self.fromdt.astimezone(pytz.utc).timestamp()/60)
+        to_utc = int(self.todt.astimezone(pytz.utc).timestamp()/60)
+        sql = f"""SELECT
+                    t.utcstart,
+                  t.localstart,
+                  p.year,
+                  p.month,
+                  p.avg_monthly_price as price
+                FROM edw.ts_times t
+                LEFT JOIN (
+                  SELECT
+                    MIN(utcstart) as utcmonth,
+                    YEAR(localstart) AS year,
+                    MONTH(localstart) AS month,
+                    AVG(price) AS avg_monthly_price
+                  FROM edw.ts_prices
+                  WHERE tsid = 19 
+                  GROUP BY YEAR(localstart), MONTH(localstart)
+                ) p
+                  ON t.startmonth = p.utcmonth
+                WHERE t.utcstart>={from_utc} and t.utcstart < {to_utc}
+                ORDER BY t.localstart"""
+        return sql
+
+    def evaluate_digital_meter(self, ts_id):
+        """
+        Evaluate the timeseries based on the provided ts_id.
+        """
+        # Placeholder for timeseries evaluation logic
+        # This should be replaced with actual evaluation logic
+        avg_epex_sql = self._get_avg_epex()
+        sql = (f"""select m.utcstart,p.price*m.offtake as offtake_cost, p.price*m.injection as injection_cost from 
+               ts_digital_meter m join ({avg_epex_sql}) p
+               on m.utcstart = p.utcstart
+               where m.tsid = {ts_id} and m.utcstart >= {int(self.fromdt.timestamp()/60)} and m.utcstart < {int(self.todt.timestamp()/60)}
+               order by m.utcstart""")
+        df = self.database.query(sql, timecols=['utcstart'])
+        return df
+
     def evaluate(self):
+        from_utc = int(self.fromdt.astimezone(pytz.utc).timestamp()/60)
+        to_utc = int(self.todt.astimezone(pytz.utc).timestamp()/60)
+
+        self.evaluate_digital_meter(ts_id=2)
+        self.evaluate_digital_meter(ts_id=3)
 
         df = self.database.query("SELECT * FROM ids_timeseries")
         print(df)
