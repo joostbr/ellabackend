055f95ce248757422f16e58f787725dd5bd96efa
Joost Bruneel <joost.bruneel@gmail.com>
2025-05-12 09:39:04 +0200

    updates

--- frontend/src/app/settings/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function TestComponent() {
-    return (
-      <div className="bg-red-500 text-white p-4">
-        If you see a red background, Tailwind is working!
-      </div>
-    )
-  }
